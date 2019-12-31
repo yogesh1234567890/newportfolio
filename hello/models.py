a08404c2222f584364bb0a9ffec472e21dd59241
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2019-12-31 15:52:58 +0545

    db configuration

--- hello/models.py
+++ hello/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Contact(models.Model):
     class Meta:
-        db_table="tbl_contact"
+        db_table="tbl_contacts"
         verbose_name_plural="Contact"
     name=models.CharField(max_length=50)
     email=models.EmailField(max_length=50)
     subject=models.CharField(max_length=100)
     message=models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+    
 
