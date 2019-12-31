c0c119351c76b8bac503f6318458fe413985ece6
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2019-12-31 21:28:31 +0545

    views

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from hello.models import Contact
 
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        index=Contact()
+        index.name=request.POST['name']
+        index.email=request.POST['email']
+        index.subject=request.POST['subject']
+        index.message=request.POST['message']
+        index.save()
+        return redirect ('/portfolio/')
+
     return render(request, "index.html")
