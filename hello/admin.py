0197ead0a653df7700dfc031a552408782f9fcfb
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2019-12-31 14:09:26 +0545

    migrations try1

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from hello.models import Contact
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    def contact(obj):
+        return obj
+
+    list_display=['name','email','subject','message']
--- hello/models.py
+++ hello/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Contact(models.Model):
+    class Meta:
+        db_table="tbl_contact"
+        verbose_name_plural="Contact"
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    subject=models.CharField(max_length=100)
+    message=models.TextField(max_length=500)
+
